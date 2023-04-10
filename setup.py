e91d646e3abb368b3edcdfa9f4c545710590f3b5
Xing Han Lu <21180505+xhluca@users.noreply.github.com>
2023-04-10 02:27:18 +0000

    Improve setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as fp:
 setup(
     name="statcan-dialogue-dataset",
     version=version["__version__"],
-    author="Xing Han Lu, Siva Reddy, Harm De Vries",
+    author="Xing Han Lu, Siva Reddy, Harm de Vries",
     author_email="statcan.dialogue.dataset@mila.quebec",
     url="https://github.com/McGill-NLP/statcan-dialogue-dataset",
     description="The Statcan Dialogue Dataset",
@@ -25,4 +25,12 @@ setup(
     extras_require={
         "dev": ["black", "wheel"]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    # Cast long description to markdown
+    long_description_content_type="text/markdown",
 )
